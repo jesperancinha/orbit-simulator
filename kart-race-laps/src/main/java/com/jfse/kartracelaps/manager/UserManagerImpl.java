ac6b33741f19ff9588eb956c0d07332e48608626
Joao <jofisaes@hotmail.com>
2016-04-09 14:09:26 +0200

    JFSE-LAPS-01 Skeleton definition

---
+++ kart-race-laps/src/main/java/com/jfse/kartracelaps/manager/UserManagerImpl.java
@@ -0,0 +1,7 @@
+package com.jfse.kartracelaps.manager;
+
+/**
+ * Created by joaofilipesabinoesperancinha on 09-04-16.
+ */
+public class UserManagerImpl implements UserManager {
+}
---
+++ kart-race-laps/src/main/java/com/jfse/kartracelaps/objects/DriverImpl.java
@@ -0,0 +1,7 @@
+package com.jfse.kartracelaps.objects;
+
+/**
+ * Created by joaofilipesabinoesperancinha on 09-04-16.
+ */
+public class DriverImpl implements Driver{
+}
