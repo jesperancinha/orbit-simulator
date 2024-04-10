7e0cfbd62b404711af513758858447383d26f8ac
jesperancinha <jofisaes@gmail.com>
2024-04-10 10:39:10 +0200

    Installs pico-cli instead

--- orbit-simulator-csv/src/main/java/org/jesperancinha/orbitsimulator/OrbitSimulatorMain.java
+++ orbit-simulator-csv/src/main/java/org/jesperancinha/orbitsimulator/OrbitSimulatorMain.java
@@ -1,15 +1,9 @@
 package org.jesperancinha.orbitsimulator;
 
 import org.jesperancinha.orbitsimulator.exceptions.PlanetOutOfOrbitException;
-import org.jesperancinha.orbitsimulator.manager.OrbitManager;
-import org.jesperancinha.orbitsimulator.manager.OrbitManagerImpl;
-import org.jesperancinha.orbitsimulator.results.Result;
 import org.jesperancinha.orbitsimulator.options.OrbitSimulatorOptions;
-import com.opencsv.CSVWriter;
-import org.kohsuke.args4j.CmdLineException;
-import org.kohsuke.args4j.CmdLineParser;
+import picocli.CommandLine;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
@@ -17,53 +11,8 @@ import java.util.concurrent.ExecutionException;
  *
  */
 public class OrbitSimulatorMain {
-
-    public static final String PASSINGTIME = "passingtime";
-    public static final String KART = "planet";
-
-    public static void main(String[] args) throws CmdLineException,
-            InterruptedException, //
-            ExecutionException, //
-            PlanetOutOfOrbitException, IOException //
-    {
+    public static void main(String[] args) throws PlanetOutOfOrbitException, ExecutionException, InterruptedException, IOException {
+        new CommandLine(new OrbitSimulatorOptions()).execute(args);
         final OrbitSimulatorOptions options = new OrbitSimulatorOptions();
-        final CmdLineParser parser = new CmdLineParser(options);
-        parser.parseArgument(args);
-        final String[] names = options.getNames().split(",");
-        String[] planetIds = null;
-        if (options.getKartIds() != null) {
-            planetIds = options.getKartIds().split(",");
         }
-
-        final OrbitManager raceManager = new OrbitManagerImpl(
-                options.getMinSimulationTime(), //
-                options.getMaxSimulationTime(), //
-                options.getnLaps(), //
-                options.getTrackLength() //
-        );
-
-        if (planetIds == null) {
-            for (int i = 0; i < names.length; i++) {
-                raceManager.addDriver(names[i], i);
-            }
-        } else {
-            for (int i = 0; i < names.length; i++) {
-                raceManager.addDriver(names[i], Integer.parseInt(planetIds[i]));
-            }
-        }
-        raceManager.start();
-
-        final String outputFile = options.getOutputFile();
-        writeResultsToCsvFile(raceManager, outputFile);
-    }
-
-    private static void writeResultsToCsvFile(OrbitManager raceManager, String outputFile) throws IOException {
-        final CSVWriter writer = new CSVWriter(new FileWriter(outputFile));
-        writer.writeNext(new String[]{KART, PASSINGTIME});
-        for (Result result : raceManager.getResults()) {
-            writer.writeNext(new String[]{result.getPlanetNumber().toString(), result.getTimeStampString()});
-        }
-        writer.flush();
-        writer.close();
-    }
 }
--- orbit-simulator-csv/src/main/java/org/jesperancinha/orbitsimulator/options/OrbitSimulatorOptions.java
+++ orbit-simulator-csv/src/main/java/org/jesperancinha/orbitsimulator/options/OrbitSimulatorOptions.java
@@ -1,31 +1,43 @@
 package org.jesperancinha.orbitsimulator.options;
 
-import org.kohsuke.args4j.Option;
+import com.opencsv.CSVWriter;
+import org.jesperancinha.orbitsimulator.manager.OrbitManager;
+import org.jesperancinha.orbitsimulator.manager.OrbitManagerImpl;
+import org.jesperancinha.orbitsimulator.results.Result;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+import static picocli.CommandLine.Option;
 
 /**
  * Created by joaofilipesabinoesperancinha on 09-04-16.
  */
-public class OrbitSimulatorOptions {
+public class OrbitSimulatorOptions implements Callable<Integer> {
 
-    @Option(name = "--names-comma-separated", aliases = "-n", required = true)
+    private static final String PASSINGTIME = "passingtime";
+    private static final String KART = "planet";
+
+    @Option(names = {"--names-comma-separated", "-n"}, required = true)
     private String names;
 
-    @Option(name = "--planet-ids-comma-separated", aliases = "-k", required = false)
+    @Option(names = {"--planet-ids-comma-separated", "-k"})
     private String planetIds = null;
 
-    @Option(name = "--min-simulation-time", aliases = "-min", required = true)
+    @Option(names = {"--min-simulation-time", "-min"}, required = true)
     private Integer minSimulationTime;
 
-    @Option(name = "--max-simulation-time", aliases = "-max", required = true)
+    @Option(names = {"--max-simulation-time", "-max"}, required = true)
     private Integer maxSimulationTime;
 
-    @Option(name = "--number-of-laps", aliases = "-laps", required = true)
+    @Option(names = {"--number-of-laps", "-laps"}, required = true)
     private Integer nLaps;
 
-    @Option(name = "--track-length", aliases =  "-t", required = false)
+    @Option(names = {"--track-length", "-t"})
     private Integer trackLength;
 
-    @Option(name = "--output-file", aliases = "-of", required = true)
+    @Option(names = {"--output-file", "-of"}, required = true)
     private String outputFile;
 
     public String getKartIds() {
@@ -54,6 +66,47 @@ public class OrbitSimulatorOptions {
 
     public String getOutputFile() {
         return outputFile;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        final String[] names = getNames().split(",");
+        String[] planetIds = null;
+        if (getKartIds() != null) {
+            planetIds = getKartIds().split(",");
+        }
+
+        final OrbitManager raceManager = new OrbitManagerImpl(
+                getMinSimulationTime(), //
+                getMaxSimulationTime(), //
+                getnLaps(), //
+                getTrackLength() //
+        );
+
+        if (planetIds == null) {
+            for (int i = 0; i < names.length; i++) {
+                raceManager.addDriver(names[i], i);
+            }
+        } else {
+            for (int i = 0; i < names.length; i++) {
+                raceManager.addDriver(names[i], Integer.parseInt(planetIds[i]));
+            }
+        }
+        raceManager.start();
+
+        final String outputFile = getOutputFile();
+        writeResultsToCsvFile(raceManager, outputFile);
+        return 0;
+    }
+
+    private static void writeResultsToCsvFile(OrbitManager raceManager, String outputFile) throws IOException {
+        final CSVWriter writer = new CSVWriter(new FileWriter(outputFile));
+        writer.writeNext(new String[]{KART, PASSINGTIME});
+        for (Result result : raceManager.getResults()) {
+            writer.writeNext(new String[]{result.getPlanetNumber().toString(), result.getTimeStampString()});
+        }
+        writer.flush();
+        writer.close();
     }
 }
 
